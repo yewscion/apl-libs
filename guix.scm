de19e93b7d04570ffd58106b5761e2a110c28338
Christopher Rodriguez <yewscion@gmail.com>
2022-09-05 22:51:01 -0400

    Fixed guix.scm to actually work for shell, albeit with extra pkgs.

--- guix.scm
+++ guix.scm
@@ -1,4 +1,3 @@
-;;; Variables: apl-libs project-homepage project-synopsis
 (use-modules
  ;;; These are my commonly needed modules; remove unneeded ones.
  (guix packages)
@@ -9,8 +8,7 @@
  (gnu packages autotools)
  (gnu packages pkg-config)
  (gnu packages texinfo)
- (gnu packages guile)
- (gnu packages java)
+ (gnu packages tex)
  (guix gexp))
 
 (package
@@ -27,45 +25,17 @@
      #:phases
      (modify-phases
       %standard-phases
-      ;; This allows the paths for guile and java to be embedded in the scripts
-      ;; in bin/
-      (add-before
-       'patch-usr-bin-file 'remove-script-env-flags
-       (lambda* (#:key inputs #:allow-other-keys)
-         (substitute*
-          (find-files "./bin")
-          (("#!/usr/bin/env -S guile \\\\\\\\")
-           "#!/usr/bin/env guile \\")
-          (("\"java")
-           (string-append "\"" (search-input-file inputs "/bin/java"))))))
-      ;; Java and Guile programs don't need to be stripped.
+      ;; APL Libraries programs don't need to be stripped.
       (delete 'strip))))
-  (native-inputs (list autoconf automake pkg-config texinfo
-                       `(,openjdk17 "jdk")))
-  (inputs (list guile-3.0-latest))
-  (synopsis "project-synopsis")
+  (native-inputs (list autoconf automake pkg-config texinfo))
+  (inputs (list texlive-base texlive-tex-texinfo))
+  (synopsis "A Personal Set of APL Libraries")
   (description
    (string-append
-    "This is a longer description of the project, which is wrapped using the "
-    "string-append function and trailing spaces.."))
+    "The Yewscion APL Libs is my personal set of APL Libraries."))
   (home-page
-   "project-homepage")
+   "https://sr.ht/~yewscion/yewscion-apl-libs/")
   (license license:agpl3+))
-
-      ;;;; Here is an example of one way to patch a script that references an
-      ;;;; installed jarfile to work with the store.
-      ;; (add-after
-      ;;  'install 'subjars
-      ;;  (lambda* (#:key outputs #:allow-other-keys)
-      ;;    (let* ((out (assoc-ref outputs "out"))
-      ;;           (bin (string-append out "/bin"))
-      ;;           (share (string-append out "/share")))
-      ;;      (substitute*
-      ;;       (string-append bin
-      ;;                      "/script")
-      ;;       (("/usr/local/share/java/apl-libs.jar")
-      ;;        (string-append share
-      ;;                       "/java/apl-libs.jar"))))))
 
 ;; Local Variables:
 ;; mode: scheme
